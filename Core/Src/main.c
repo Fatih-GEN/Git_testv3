2177933af5a85df436b80ffdea234fa84a17364c
Fatih-GEN <97450994+Fatih-GEN@users.noreply.github.com>
2022-01-12 15:29:15 +0300

    commitxx

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,6 +92,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+	  HAL_Delay(1000);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+	  HAL_Delay(1000);
+
 	  system_buzzer_on();
 
     /* USER CODE END WHILE */
