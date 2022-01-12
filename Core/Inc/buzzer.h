60a930b1850942e7a4b6dfb3b5c4605d50079820
Fatih-GEN <97450994+Fatih-GEN@users.noreply.github.com>
2022-01-12 12:34:24 +0300

    Branch1 has added buzzer.c and buzzer.h// commit branch1

---
+++ Core/Inc/buzzer.h
@@ -0,0 +1,13 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: fatih.colak
+ */
+
+#ifndef INC_BUZZER_H_
+#define INC_BUZZER_H_
+
+
+
+#endif /* INC_BUZZER_H_ */
---
+++ Core/Src/buzzer.c
@@ -0,0 +1,8 @@
+/*
+ * buzzer.c
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: fatih.colak
+ */
+
+
